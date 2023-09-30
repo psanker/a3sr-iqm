aac293ca013c8007b6a3bffafba7bdf53e7caca0
Patrick Anker <patricksanker@gmail.com>
2023-09-30 14:27:48 -0400

    Start HW3

---
+++ homework3/analysis.R
@@ -0,0 +1,10 @@
+# written and runs on R 4.2.1
+#
+# required packages:
+# - tidytable (tidyverse but with data.table backend)
+# - here
+# - ggplot2
+#
+# The assumption is that this file was run inside a folder called "homework3"
+# within an Rproj directory. Link to GitHub: <https://github.com/psanker/a3sr-iqm>
+
